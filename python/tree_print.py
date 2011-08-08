ceb7342316c9ee9d412f24caddb0961fb3f3b034
Seth Holloway <smholloway@gmail.com>
2011-08-08 15:52:55 -0500

    added version information to tree_print file

--- python/tree_print.py
+++ python/tree_print.py
@@ -2,18 +2,18 @@
 filename:    tree_print.py
 author:      Seth Holloway
 description: Given an unordered set of Nodes, pretty print them (with tab characters showing depth)
+version:     2
 """
 
 class Node:
 	def __init__(self, label, id, parent_id):
-		self.label=label
-		self.id=id
-		self.parent_id=parent_id
+		self.label     = label
+		self.id        = id
+		self.parent_id = parent_id
 
-def pp(nodes):
-	pp_stack = []
+def tree_print(nodes):
 	for node in nodes:
-		if node.parent_id == 0:
+		if node.parent_id == 0: # root node
 			print node.label
 			print_below(nodes, node, 1)
 	print ""
@@ -27,8 +27,8 @@ def print_below(nodes, parent_node, depth):
 			print_below(nodes, node, depth+1)
 
 # Test a few cases
-pp( [ Node("A", 1, 0), Node("B", 2, 0), Node("C", 3, 0) ] )
-pp( [ Node("A", 1, 0), Node("B", 2, 1), Node("C", 3, 2) ] )
-pp( [ Node("A", 1, 0), Node("B", 2, 0), Node("C", 3, 2) ] )
-pp( [ Node("A", 1, 0), Node("B", 2, 3), Node("C", 3, 1) ] )
-pp( [ Node("A", 1, 0), Node("B", 2, 1), Node("C", 3, 2), Node("D", 4, 3), Node("E", 5, 4), Node("F", 6, 5) ] )
+tree_print( [ Node("A", 1, 0), Node("B", 2, 0), Node("C", 3, 0) ] )
+tree_print( [ Node("A", 1, 0), Node("B", 2, 1), Node("C", 3, 2) ] )
+tree_print( [ Node("A", 1, 0), Node("B", 2, 0), Node("C", 3, 2) ] )
+tree_print( [ Node("A", 1, 0), Node("B", 2, 3), Node("C", 3, 1) ] )
+tree_print( [ Node("A", 1, 0), Node("B", 2, 1), Node("C", 3, 2), Node("D", 4, 3), Node("E", 5, 4), Node("F", 6, 5) ] )
