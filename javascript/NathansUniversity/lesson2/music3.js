2c546ef42b8f8b4fe110903a368811f750e58b03
Seth Holloway <smholloway@gmail.com>
2012-05-13 17:57:46 -0500

    fixed typos and spacing

--- javascript/NathansUniversity/lesson2/music3.js
+++ javascript/NathansUniversity/lesson2/music3.js
@@ -9,21 +9,20 @@ var compile = function (musexpr, time) {
       start: time
     }];
   } else if ( musexpr.tag === 'seq' ) {
-    return compileFree( musexpr.left, time).concat( compileFree( musexpr.right, endTime(time, musexpr.left)) );
+    return compile( musexpr.left, time).concat( compile( musexpr.right, endTime(time, musexpr.left)) );
   }
-}
+};
 
 var endTime = function (time, expr) {
-		if ( expr.tag === 'note' ) {
-				return time + expr.dur;
-		} 
-		return endTime( endTime(time, expr.left), expr.right );
+  if ( expr.tag === 'note' ) {
+    return time + expr.dur;
+  } 
+  return endTime( endTime(time, expr.left), expr.right );
 };
 
 
 var melody1_mus = { tag: 'note', pitch: 'a4', dur: 125 };
-var melody1_note = [ 
-    { tag: 'note', pitch: 'a4', start: 0, dur: 125 } ];
+var melody1_note = [ { tag: 'note', pitch: 'a4', start: 0, dur: 125 } ];
 var melody2_mus = 
     { tag: 'seq',
       left: 
