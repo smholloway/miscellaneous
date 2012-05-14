35ded6d63d4ee62afc258862bf00129161d66f6e
Seth Holloway <smholloway@gmail.com>
2012-05-13 20:33:44 -0500

    moved NathansUniversity to its own repo

--- javascript/NathansUniversity/lesson2/music2.js
+++
@@ -1,7 +0,0 @@
-var endTime = function (time, expr) {
-    if ( expr.tag === 'note' ) {
-        return time + expr.dur;
-    } else if ( expr.tag === 'seq' ) {
-        return endTime(time, expr.left) + endTime(time, expr.right);
-    }
-};
--- javascript/NathansUniversity/lesson2/music4.js
+++
@@ -1,3 +0,0 @@
-var playMUS = function(musSong) {
-    playNOTE(compile(musSong));
-};
