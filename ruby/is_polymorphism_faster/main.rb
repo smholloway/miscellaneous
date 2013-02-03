4d1720eb79780c41d0c7f1939250e80ebfe8686e
Seth Holloway <smholloway@gmail.com>
2013-02-03 13:25:35 -0600

    added explanatory comments

--- ruby/is_polymorphism_faster/main.rb
+++ ruby/is_polymorphism_faster/main.rb
@@ -1,3 +1,9 @@
+# Testing the speed of polymorphism in Ruby. This program is based on the Wikipedia article
+# http://en.wikipedia.org/wiki/Polymorphism_in_object-oriented_programming
+
+# To run: ruby main.rb <number of runs to time (optional)>
+# Example: ruby main.rb 5
+
 class Animal
   def initialize(name)
     @name = name
